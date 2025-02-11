d64db0234261f4c2e22a7a8fb85a2c0fa6d38c99
glebkostin <glebkos777@gmail.com>
2025-02-12 00:35:41 +0300

    Добавил хедер

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,16 @@ import './App.css';
 import { MainPage } from './Pages/mainPage/MainPage.tsx';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import { Manager } from './Pages/managerPage/Manager.tsx';
+import {PageWrapper} from "./Pages/PageWrapper/PageWrapper.tsx";
 
 function App() {
 
   return (
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/manager/:id" element={<Manager />}/>
+            <Route path="/" element={<PageWrapper><MainPage /></PageWrapper>} />
+            <Route path="/manager/:id" element={<PageWrapper><Manager /></PageWrapper>}/>
+            <Route path="/settings" element={<PageWrapper><Manager /></PageWrapper>}/>
         </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/Pages/PageWrapper/PageWrapper.tsx
@@ -0,0 +1,12 @@
+import {ReactElement} from "react";
+import {Header} from "../../Components/Header/Header.tsx";
+
+interface PageWrapperProps {
+    children?: ReactElement;
+}
+export const PageWrapper = ({children}: PageWrapperProps) => {
+    return (<>
+        <Header />
+        {children}
+    </>);
+};
