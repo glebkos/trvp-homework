81396bcef720d97c47552908c40c2920766131e4
glebkostin <glebkos777@gmail.com>
2025-02-16 23:40:47 +0300

    Добавил ручку взятия настроек

--- frontend/src/Pages/Settings/Settings.tsx
+++ frontend/src/Pages/Settings/Settings.tsx
@@ -1,9 +1,21 @@
 import './Settings.css';
 import { VerticalList } from '../../Components/List/VerticalList.tsx';
 import { ProfileItem } from '../../Components/ProfileItem/ProfileItem.tsx';
+import {useEffect, useState} from "react";
+import {fetchData} from "../../helpers/fetchHelpers.ts";
 
 export const Settings = () => {
-    const mockProfiles = [ { name: 'profile1' }, { name: 'profile2' } ];
+    const [N, setN] = useState();
+    const [profiles, setProfiles] = useState([{}]);
+
+    useEffect(() => {
+        fetchData('settings').then(data => setN(data[0].n));
+    }, []);
+
+    useEffect(() => {
+        fetchData('profiles').then(data => setProfiles(data));
+    }, []);
+
     return (
         <div className="settings__root">
             <div className="settings__window">
@@ -13,11 +25,11 @@ export const Settings = () => {
                 <div className="settings__content">
                     <div className="settings__block">
                         <span className="settings__block-title">Ограничение на количество юрлиц(N)</span>
-                        <input type="text" className="settings__block-input input"/>
+                        <input type="text" className="settings__block-input input" value={N}/>
                     </div>
                     <div className="settings__block">
                         <span className="settings__block-title">Профили обслуживания</span>
-                        <VerticalList Entity={ProfileItem} items={mockProfiles}/>
+                        <VerticalList Entity={ProfileItem} items={profiles}/>
                         <div className="setting__profile-add-block">
                             <input type="text" className="input"/>
                             <button className="button">Добавить</button>
