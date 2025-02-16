519b56c797f75f8b385ed2b04558d60b19dfa3e6
glebkostin <glebkos777@gmail.com>
2025-02-16 16:56:28 +0300

    Добавил ручки менеджера

---
+++ backend/controllers/managerController.ts
@@ -0,0 +1,44 @@
+const pool = require('../config/db.ts');
+
+exports.managerGet = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const result = (await pool.query('SELECT * FROM manager WHERE manager_id=$1', [id]));
+        const clients = await pool.query('SELECT * FROM clients WHERE clients_manager=$1', [id]);
+        result.rows[0].clients = clients ? clients.rows : {};
+        res.status(200).json(result.rows);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+exports.managerUpdate = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const {name, profile} = req.body;
+        const result = await pool.query('UPDATE manager SET manager_name=$1, manager_profile=$2 WHERE manager_id=$3', [name, profile, id]);
+        res.status(200).json(result.rows);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+exports.managerAdd = async (req, res) => {
+    try {
+        const {name, profile} = req.body;
+        const result = await pool.query('INSERT INTO manager (manager_name, manager_profile) VALUES($1, $2)', [name, profile]);
+        res.status(200).json(result.rows);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+exports.managerDelete = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const result = await pool.query('DELETE FROM manager WHERE manager_id=$1', [id]);
+        res.status(200).json(result.rows);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -2,11 +2,17 @@ const express = require('express');
 const router = express.Router();
 const settingsController = require('../controllers/settingsController.ts');
 const profileController = require('../controllers/profileController.ts');
+const managerController = require('../controllers/managerController.ts');
 
 router.get('/settings', settingsController.settingsGet);
 router.post('/settings', settingsController.settingsUpdate);
 
 router.get('/profiles', profileController.profilesGet);
 router.post('/profiles', profileController.profilesAdd);
+
+router.get('/manager/:id', managerController.managerGet);
+router.post('/manager/create', managerController.managerAdd);
+router.post('/manager/:id/update', managerController.managerUpdate);
+router.delete('/manager/:id', managerController.managerDelete);
 
 module.exports = router;
